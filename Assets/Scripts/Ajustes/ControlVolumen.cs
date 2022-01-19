088103b7533033b6c8f2f6a7e9620f781ecf59b7
Daniel Correa <dcorrealenis@gmail.com>
2022-01-19 12:10:44 -0500

    Funcionalidad Volumen
    La funcionalidad de subir el volumen de la musica ya quedo lista y se mantiene en todas las escenas, se debe mirar si los efectos tambien se deberian oculatar

---
+++ Assets/Scripts/Ajustes/ControlVolumen.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlVolumen : MonoBehaviour
+{      
+    public Slider sliderVolumen;
+
+    public GameObject[] Sonidos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sonidos=GameObject.FindGameObjectsWithTag("Sonidos");
+        sliderVolumen.value= PlayerPrefs.GetFloat("VolumenAGuardar",1f);
+
+    }
+
+    void Update()
+    {
+
+        foreach(GameObject sonido in Sonidos)
+        {
+
+            sonido.GetComponent<AudioSource>().volume=sliderVolumen.value;
+
+
+
+        }
+
+
+    }
+
+    public void GuardarVolumen()
+    {
+
+        PlayerPrefs.SetFloat("VolumenAGuardar",sliderVolumen.value);
+
+    }
+
+    
+}
