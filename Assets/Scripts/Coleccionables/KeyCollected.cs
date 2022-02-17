662ce59f5d884ce8cb549fc2ab389f90cafda6e1
Daniel Correa <dcorrealenis@gmail.com>
2022-02-17 09:48:00 -0500

    Panel de preguntas
    Se añadio el panel de preguntas y se activa cuando se coge la llave, se coloco una pregunta de ejemplo de momento

--- Assets/Scripts/Coleccionables/KeyCollected.cs
+++ Assets/Scripts/Coleccionables/KeyCollected.cs
@@ -6,29 +6,35 @@ public class KeyCollected : MonoBehaviour
 {
     public AudioSource clip;
     public GameObject Key;
+    public GameObject PanelPreguntas;
     public static bool IsKeyCollected=false;
     
     private void OnTriggerEnter2D(Collider2D Collision) {
 
         if(Collision.CompareTag("Player")){
 
-            IsKeyCollected=true;
-            GetComponent<SpriteRenderer>().enabled=false;
-            
-            Key.gameObject.SetActive(true);
-            Destroy(gameObject,0.5f);
+            PanelPreguntas.gameObject.SetActive(true);
+            Time.timeScale=0;
 
-            clip.Play();
+            
 
 
         }
 
     }
 
+    public void IsCorrect()
+    {
+            
+        PanelPreguntas.gameObject.SetActive(false);
+        Time.timeScale=1;
 
-    private void Preguntas(){
+        IsKeyCollected=true;
+        GetComponent<SpriteRenderer>().enabled=false;
+            
+        Key.gameObject.SetActive(true);
+        Destroy(gameObject,0.5f);
 
-
-        
+        clip.Play();
     }
 }
